4d0d2ffdca234e5950b75c02b27cf28334a99a34
Docker <docker@example.com>
2023-01-28 09:25:02 +0000

    custom parser
    Signed-off-by: Docker <docker@example.com>

--- lib/compiler.ml
+++ lib/compiler.ml
@@ -1,15 +1,5 @@
-open Parsetree 
-
-
 let process chan =
-  let linebuf = Lexing.from_channel chan in
-  try
-    (* Run the parser on this line of input. *)
-    let x = Parser.prog Lexer.token linebuf in
-    let s = show_prog x in
-    print_endline s;
-  with
-  | Lexer.Error msg ->
-      Printf.fprintf stderr "%s%!" msg
-  | Parser.Error ->
-      Printf.fprintf stderr "At offset %d: syntax error.\n%!" (Lexing.lexeme_start linebuf)
+  match Parsing_.parse chan with
+  | Ok x -> print_endline (Parsing_.Parsetree.show_prog x)
+  | Error x -> print_endline x
+
---
+++ lib/parsing/parsing_.ml
@@ -0,0 +1,12 @@
+module Parsetree = Parsetree
+
+let parse c =
+  let buf = Lexing.from_channel c in
+  try
+    let x = Parser.prog Lexer.token buf in
+    Ok x
+  with
+  | Lexer.Error msg -> Error msg
+  | Parser.Error ->
+      let msg = Printf.sprintf "At offset %d: syntax error.\n%!" (Lexing.lexeme_start buf) in
+      Error msg
