f6d66acd186e436218b86a1a0a1bbc327af18297
Furetur <furetur@gmail.com>
2024-03-19 11:25:49 +0300

    #16: Organize tests into test suites, add new tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,5 +4,8 @@ set -e
 export TEST_SUITE_ROOT=test/exec
 export COMPILER_BINARY=./_build/default/bin/herbc.exe
 
+N_PARALLEL_WORKERS=6
+
 dune runtest
-pytest .
+# requires the `pytest-xdist` package
+pytest -n $N_PARALLEL_WORKERS .
